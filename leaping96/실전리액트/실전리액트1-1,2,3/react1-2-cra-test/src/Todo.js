52f35689f6ea372addb8194d084c906cf707e618
leaping <leaping96@ajou.ac.kr>
2021-09-15 12:46:02 +0900

    leaping96/ 9월 3주차-1

---
+++ leaping96/실전리액트/실전리액트1-1,2,3/react1-2-cra-test/src/Todo.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Todo({ title }) {
+    return <div className="css-class">{title}</div>;
+}
--- leaping96/실전리액트2-3/test.js
+++
@@ -1,7 +0,0 @@
-function Something() {
-    this.value = 1;
-    setInterval(function increase() {
-        this.value++;
-    }, 1000);
-}
-const obj = new Something();
