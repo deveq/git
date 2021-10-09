be5bcdbde51088b8b0909f39ba499dbb0f5bcd16
deveq <deveq@kakao.com>
2021-10-10 00:24:00 +0900

    chap04

--- JIN-YOUNG JANG/react/movie/src/App.js
+++ JIN-YOUNG JANG/react/movie/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Food({name, rating}) {
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <h4>{rating}/5.0</h4>
+    </div>
+  )
+}
+
+Food.propTypes = {
+  name : PropTypes.string.isRequired,
+  rating : PropTypes.number
+}
+
+const foodILike = [
+  {
+    name : 'kimchi',
+    id : 1,
+    rating : 5
+  }, {
+    name : 'samgyeopsal',
+    id : 2,
+    rating : 4.5
+  }, {
+    name : 'bibimbap',
+    id : 3,
+    rating : 4.8
+  }, {
+    name : 'doncasu',
+    id : 4,
+    rating : 4.7
+  }, {
+    name : 'kimbap',
+    id : 5,
+    rating : 5
+  }
+];
 
 function App() {
+  console.log(foodILike)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      {
+        foodILike.map(dish => <Food name={dish.name} key={dish.id} rating={dish.rating}/>)
+      }
     </div>
   );
 }
